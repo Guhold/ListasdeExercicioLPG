229f7385fde68c4122766db3998e2ab3abf9a424
Guhold <gustavoholderegger@gmail.com>
2025-09-02 15:29:20 -0300

    Adição das quebras de linha (\n)

--- Lista1-SelecaoeRepeticao/exercicio1.c
+++ Lista1-SelecaoeRepeticao/exercicio1.c
@@ -10,25 +10,25 @@ int main(){
         printf("O ponto esta na origem\n");
         return 0;
     } if (x == 0){
-        printf ("O ponto esta no eixo x");
+        printf ("O ponto esta no eixo x\n");
         return 0;
     } if (y == 0){
-        printf ("O ponto esta no eixo y");
+        printf ("O ponto esta no eixo y\n");
         return 0;
     }
     if (x > 0){
         if (y > 0) {
-            printf ("O ponto esta no primeiro quadrante");
+            printf ("O ponto esta no primeiro quadrante\n");
         } else {
-            printf ("O ponto esta no quarto quadrante");
+            printf ("O ponto esta no quarto quadrante\n");
         }
         return 0;
     }
     else {
         if (y > 0) {
-            printf("O ponto esta no segundo quadrante");
+            printf("O ponto esta no segundo quadrante\n");
         } else {
-            printf("O ponto esta no terceiro quadrante");
+            printf("O ponto esta no terceiro quadrante\n");
         }
         return 0;
     }
